506e0a4d04dbc8af7fb7822d8f2bc94a6fde3cec
Conor Camplisson <conor.camplisson@gmail.com>
2024-09-02 14:00:55 -0700

    add authoritative _version.py file

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
+PACKAGE_NAME = 'cellcyclenet'
+
+def read_version():
+    '''Dynamically read version from authoritative _version.py file.'''
+    version = {}
+    with open(os.path.join(PACKAGE_NAME, '_version.py')) as f:
+        exec(f.read(), version)
+    return version['__version__']
+
 setup(
-    name="cellcyclenet",
-    version="0.1.0",
+    name=PACKAGE_NAME,
+    version=read_version(),
     packages=find_packages(),
-    include_package_data=True,  # Include package data as specified in MANIFEST.in
+    include_package_data=True,  # iInclude package data as specified in MANIFEST.in
     package_data={
-        'cellcyclenet': ['models/*.pt'],  # Include all .pt files in models/
+        PACKAGE_NAME: ['models/*.pt'],  # Include all .pt files in models/
     },
     install_requires=[
         # List your package dependencies here, e.g.,
